a862fa274503973ce61147e62e7d6acaaed11829
MIDU-SER <98685114+MIDU-SER@users.noreply.github.com>
2022-03-19 14:29:47 +0530

    Add files via upload

---
+++ Utilis/StringSession.js
@@ -0,0 +1,51 @@
+/* 
+
+Copyright (C) 2022 MIDLAJ 
+
+*/
+
+const fs = require('fs');
+
+class StringSession {
+
+    constructor() {
+
+    }
+
+    deCrypt(string = undefined) {
+
+        if ('MJ_CODE' in process.env && string === undefined) {
+
+            string = process.env.STRING_SESSION;
+
+        } else if (string !== undefined) {
+
+            if (fs.existsSync(string)) {
+
+                string = fs.readFileSync(string, {encoding:'utf8', flag:'r'});
+
+            }
+
+        }
+
+        
+
+        var split = string.split(';;;');
+
+        if (split.length >= 2) {
+
+            return JSON.parse(Buffer.from(split[split.length - 1], 'base64').toString('utf-8'));
+
+        }
+
+    }
+
+    createStringSession(dict) {
+
+        return 'ASENA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+
+    }
+
+}
+
+module.exports = StringSession;
---
+++ Utilis/index.js
@@ -0,0 +1,14 @@
+/* 
+
+Copyright (C) 2022 MIDLAJ 
+
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
